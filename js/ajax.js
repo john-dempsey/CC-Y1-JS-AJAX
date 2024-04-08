bfe1dbd5f5aa52a085a671d1641e17f7b9da3b12
John Dempsey <john.dempsey@iadt.ie>
2024-04-08 19:55:59 +0100

    Sending request with fetch() and extracting json

--- js/ajax.js
+++ js/ajax.js
@@ -2,7 +2,14 @@ let fetchPromise = fetch("./json/movies.json");
 
 console.log("Sent request...");
 
-fetchPromise.then((response) => {
+let jsonPromise = fetchPromise.then((response) => {
     console.log("...received response");
-    console.log(response);
+    console.log("Extracting JSON from request...");
+    let dataPromise = response.json();  //start encoding into an object
+    return dataPromise;
+});
+
+jsonPromise.then((data) => {
+    console.log("...extracted JSON from request");
+    console.log(data);
 });
